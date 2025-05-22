1db3f0bb717a2ab4d02b76d8b28c54eebb5579d2
abasu <amitava_basu@apple.com>
2025-05-22 16:12:41 -0500

    added comment ...

--- src/training/progression/FindMissingIP.java
+++ src/training/progression/FindMissingIP.java
@@ -2,31 +2,60 @@ package training.progression;
 
 public class FindMissingIP {
 
-    static int n = 256;
-    static int numberOfDigits = 3;
-    static int base = 256;
-    static double digitSum1 = ((n/2) * (2*0 + (n-1)*1)) * Math.pow(base, (numberOfDigits-1));
-    static double digitSum2 = ((n/2) * (2*0 + (n-1)*1)) * Math.pow(base, (numberOfDigits-1));
-    static double digitSum3 = ((n/2) * (2*0 + (n-1)*1)) * Math.pow(base, (numberOfDigits-1));
-    static double digitSum4 = ((n/2) * (2*0 + (n-1)*1)) * Math.pow(base, (numberOfDigits-1));
+    static String missingIp = "0.1.2.3";
+    static int numberOfDigits = 4;
+    static int baseForEachDigit = 256;//<-- also can be termed as number of sequence available, i.e. each digit can have value from 0 to 255
+
+    /*  Formulas:
+
+        Sum of an arithmetic progression
+        Sum = (n/2) * [2*a + (n-1)*d]
+        where ...
+        n = number of elements
+        a = value of start element/ beginning of the sequence
+        d = common difference between consecutive terms
+
+        Sum of the sums of entire sequence
+        SumOfSums = Sum * [baseForEachDigit ^ (numberOfDigits-1)]
+
+     */
+
+    static int n = baseForEachDigit; //Because total elements in the sequence is same as base, which is 256 here (from 0 to 255).
+    static int a = 0; //<-- starting element is 0
+    static int d = 1; //<-- common difference is 1
+
+    static double sumOfAllDigits = ((n/2) * (2*a + (n-1)*d));
+    static double sumOfSumsOfAllDigitsInTheEntireSequence = (sumOfAllDigits) * (int)Math.pow(baseForEachDigit, (numberOfDigits-1));
+
+    static double digitSum1 = sumOfSumsOfAllDigitsInTheEntireSequence;
+    static double digitSum2 = sumOfSumsOfAllDigitsInTheEntireSequence;
+    static double digitSum3 = sumOfSumsOfAllDigitsInTheEntireSequence;
+    static double digitSum4 = sumOfSumsOfAllDigitsInTheEntireSequence;
 
 
     public static void main(String[] args) {
-//        for (int digit4 = 0; digit4 < 256; digit4++) {
+        String[] digits = missingIp.split("\\.");
+        int ipDigit1 = Integer.parseInt(digits[3]);
+        int ipDigit2 = Integer.parseInt(digits[2]);
+        int ipDigit3 = Integer.parseInt(digits[1]);
+        int ipDigit4 = Integer.parseInt(digits[0]);
+
+        for (int digit4 = 0; digit4 < 256; digit4++) {
+            System.out.println("MSB: " + digit4);
             for (int digit3 = 0; digit3 < 256; digit3++) {
                 for (int digit2 = 0; digit2 < 256; digit2++) {
                     for (int digit1 = 0; digit1 < 256; digit1++) {
-                        if (/*digit4 == 0 && */digit3 == 11 && digit2 == 11 && digit1 == 11) continue;
-                        System.out.println(/*digit4 +"." +*/ digit3 + "." + digit2 + "." + digit1);
+                        if (digit4 == ipDigit4 && digit3 == ipDigit3 && digit2 == ipDigit2 && digit1 == ipDigit1) continue;
+//                        System.out.println(digit4 +"." + digit3 + "." + digit2 + "." + digit1);
                         digitSum1 = digitSum1 - digit1;
                         digitSum2 = digitSum2 - digit2;
                         digitSum3 = digitSum3 - digit3;
-//                        digitSum4 = digitSum4 - digit4;
-                        System.out.println("digitSum4=" + digitSum4 +" digitSum3=" + digitSum3 + " digitSum2=" + digitSum2 + " digitSum1=" + digitSum1);
+                        digitSum4 = digitSum4 - digit4;
+//                        System.out.println("digitSum4=" + digitSum4 +" digitSum3=" + digitSum3 + " digitSum2=" + digitSum2 + " digitSum1=" + digitSum1);
                     }
                 }
             }
-//        }
-        System.out.println("missing IP=" /*+ digitSum4 +"."*/ + (int)digitSum3 + "." + (int)digitSum2 + "." + (int)digitSum1);
+        }
+        System.out.println("missing IP=" + (int)digitSum4 +"." + (int)digitSum3 + "." + (int)digitSum2 + "." + (int)digitSum1);
     }
 }
--- src/training/progression/FindMissingNumber.java
+++ src/training/progression/FindMissingNumber.java
@@ -1,36 +1,55 @@
 package training.progression;
 
+/*
+Find the only missing number in all numbers between 0 & 999.
+You have a list of all numbers except one stored in a file between 0 an 999.
+They are not sorted in the file. Trace the entire file and find the missing number
+ */
+
 public class FindMissingNumber {
 
-    static int n = 10;
-    static int numberOfDigits = 3;
-    static int base = 10;
-    static int digitSum1 = ((n/2) * (2*0 + (n-1)*1)) * (int)Math.pow(base, (numberOfDigits-1));
-    static int digitSum2 = ((n/2) * (2*0 + (n-1)*1)) * (int)Math.pow(base, (numberOfDigits-1));
-    static int digitSum3 = ((n/2) * (2*0 + (n-1)*1)) * (int)Math.pow(base, (numberOfDigits-1));
+    static int missingNumber = 111; //<-- This is the missing number
+    static int numberOfDigits = 3; //<-- how many digit the number has
+    static int baseForEachDigit = 10;//<-- also can be termed as number of sequence available
 
+    /*  Formulas:
+
+        Sum of an arithmetic progression
+        Sum = (n/2) * [2*a + (n-1)*d]
+        where ...
+        n = number of elements
+        a = value of start element/ beginning of the sequence
+        d = common difference between consecutive terms
+
+        Sum of the sums of entire sequence
+        SumOfSums = Sum * [baseForEachDigit ^ (numberOfDigits-1)]
+
+     */
+
+    static int n = baseForEachDigit; //Because total elements in the sequence is same as base, which is 10 here.
+    static int a = 0; //<-- starting element is 0
+    static int d = 1; //<-- common difference is 1
+
+    static int sumOfAllDigits = ((n/2) * (2*a + (n-1)*d));
+    static int sumOfSumsOfAllDigitsInTheEntireSequence = (sumOfAllDigits) * (int)Math.pow(baseForEachDigit, (numberOfDigits-1));
+
+    static int digitSum1 = sumOfSumsOfAllDigitsInTheEntireSequence;
+    static int digitSum2 = sumOfSumsOfAllDigitsInTheEntireSequence;
+    static int digitSum3 = sumOfSumsOfAllDigitsInTheEntireSequence;
 
     public static void main(String[] args) {
-        for (int number = 0; number < 1000; number++) {
-            if (number == 7) continue;
-            int digit1 = number % 10;
-            int digit2 = (number /10) % 10;
-            int digit3 = (number /100) % 10;
-            System.out.println("number=" + number +" digit3=" + digit3 + " digit2=" + digit2 + " digit1=" + digit1);
-            digitSum1 = digitSum1 - digit1;
-            digitSum2 = digitSum2 - digit2;
-            digitSum3 = digitSum3 - digit3;
-            System.out.println(" digitSum3=" + digitSum3 + " digitSum2=" + digitSum2 + " digitSum1=" + digitSum1);
+        for (int number = 0; number < 1000; number++) {//Generate the numbers or read from the file - This means 000 to 999 <-- 3digit number, skipp the missing one
+            if (number == missingNumber) continue;//<-- for the missing number skipp, so that we can find the number
+            int digit1 = number % 10; //<-- Extract least significant digit of the current element
+            int digit2 = (number /10) % 10; //<-- Extract middle digit of the current element
+            int digit3 = (number /100) % 10; //<-- Extract most significant digit of the current element
+//            System.out.println("number=" + number +" digit3=" + digit3 + " digit2=" + digit2 + " digit1=" + digit1);
+            digitSum1 = digitSum1 - digit1; //<-- subtract value of digit1 from digitSum1
+            digitSum2 = digitSum2 - digit2; //<-- subtract value of digit2 from digitSum2
+            digitSum3 = digitSum3 - digit3; //<-- subtract value of digit3 from digitSum3
+//            System.out.println(" digitSum3=" + digitSum3 + " digitSum2=" + digitSum2 + " digitSum1=" + digitSum1);
         }
-        int missingNumber = digitSum3 * 100 + digitSum2 * 10 + digitSum1;
+        int missingNumber = digitSum3 * 100 + digitSum2 * 10 + digitSum1; //<-- re-calculate back the missing number
         System.out.println("missing number=" + missingNumber +" digit3=" + digitSum3 + " digit2=" + digitSum2 + " digit1=" + digitSum1);
     }
-
-
-
-
-
-
-
-
 }
